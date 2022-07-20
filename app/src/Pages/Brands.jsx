e0fb2c50aa18c47403535201c24fcf0a2e48776e
Rasheedsheikh <100085425+Rasheedsheikh@users.noreply.github.com>
2022-07-20 22:03:04 +0530

    Brands

---
+++ app/src/Pages/Brands.jsx
@@ -0,0 +1,565 @@
+import React from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import Navbar from '../component/Navbar'
+import "./Brands.css"
+const Brands = () => {
+
+    const nav= useNavigate()
+
+    const handleClick=()=>{
+        nav("/abel")
+    }
+  return (
+    <>
+  <Navbar/>
+    <div>
+
+{/* <div className='brandnav'>FREE OVERNIGHT SHIPPING WITHOUT MINIMUM PURCHASE</div> */}
+
+<div className='firstco'>
+    <div className='Allbran'>ALL BRANDS</div></div>
+    <div className="secondco">
+       <div className="alphabet">
+            <div><a  style={{textDecoration:"none",color:"#12284C"}} href="#A">A</a></div>
+            <div ><a  style={{textDecoration:"none",color:"#12284C"}} href="#B">B</a></div>
+            <div ><a  style={{textDecoration:"none",color:"#12284C"}} href="#C">C</a></div>
+            <div ><a  style={{textDecoration:"none",color:"#12284C"}} href="#D">D</a></div>
+            <div ><a  style={{textDecoration:"none",color:"#12284C"}} href="#E">E</a></div>
+            <div ><a  style={{textDecoration:"none",color:"#12284C"}} href="#F">F</a></div>
+            <div ><a  style={{textDecoration:"none",color:"#12284C"}} href="#G">G</a></div>
+            <div ><a  style={{textDecoration:"none",color:"#12284C"}} href="#H">H</a></div>
+            <div ><a  style={{textDecoration:"none",color:"#12284C"}} href="#I">I</a></div>
+            <div ><a  style={{textDecoration:"none",color:"#12284C"}} href="#J">J</a></div>
+            <div ><a  style={{textDecoration:"none",color:"#12284C"}} href="#K">K</a></div>
+            <div ><a  style={{textDecoration:"none",color:"#12284C"}} href="#A">L</a></div>
+            <div ><a  style={{textDecoration:"none",color:"#12284C"}} href="#A">M</a></div>
+            <div ><a  style={{textDecoration:"none",color:"#12284C"}} href="#A">N</a></div>
+            <div ><a  style={{textDecoration:"none",color:"#12284C"}} href="#A">O</a></div>
+            <div ><a  style={{textDecoration:"none",color:"#12284C"}} href="#A">P</a></div>
+            <div ><a  style={{textDecoration:"none",color:"#12284C"}} href="#A">Q</a></div>
+            <div ><a  style={{textDecoration:"none",color:"#12284C"}} href="#A">R</a></div>
+            <div ><a  style={{textDecoration:"none",color:"#12284C"}} href="#A">S</a></div>
+            <div ><a  style={{textDecoration:"none",color:"#12284C"}} href="#A">T</a></div>
+            <div ><a  style={{textDecoration:"none",color:"#12284C"}} href="#A">U</a></div>
+            <div ><a  style={{textDecoration:"none",color:"#12284C"}} href="#A">V</a></div>
+            <div ><a  style={{textDecoration:"none",color:"#12284C"}} href="#A">W</a></div>
+            <div ><a  style={{textDecoration:"none",color:"#12284C"}} href="#A">X</a></div>
+            <div ><a  style={{textDecoration:"none",color:"#12284C"}} href="#A">Y</a></div>
+            <div ><a  style={{textDecoration:"none",color:"#12284C"}} href="#A">Z</a></div>
+            <div ><a  style={{textDecoration:"none",color:"#12284C"}} href="#A">#</a></div>
+       </div>
+    </div>
+    <div className="thirdco">
+        <div className="letterbox" id="A">A</div>
+        <hr/>
+        <div className="item_box">
+            <div>
+                <div >
+                    <Link style={{textDecoration:"none"}} to="/abel">ABEL</Link></div>
+                    {/* <div  onClick={()=>{handleClick()}}>ABEL</div> */}
+                <div>AKS</div>
+                <div>Augustinus Bader</div>
+            </div>
+            <div>
+                <div>Aqua Di Parma</div>
+                <div>Alpine Beauty</div>
+                <div>Avene</div>
+            </div>
+            <div>
+                <div>African Botanics</div>
+                <div>AMASS</div>
+            </div>
+            <div>
+                <div>Agent Nateur</div>
+                <div>Anthony</div>
+            </div>
+            <div>
+                <div>AVAHA</div>
+                <div>Aromatherapy Associates</div>
+            </div>
+        </div>
+        <div className="letterbox"  id="B">B</div>
+        <hr/>
+        <div className="item_box">
+            <div>
+                <div>   <Link style={{textDecoration:"none"}} to="/babyfoot">BABY FOOT</Link></div>
+                <div>Beneath Your Mask</div>
+                <div>Bumble and bumble</div>
+            </div>
+            <div>
+                <div>Biophile</div>
+                <div>BY TERRY</div>
+                <div>BareMinerals</div>
+            </div>
+            <div>
+                <div>Bloomeffects</div>
+                <div>Beautyblender</div>
+            </div>
+            <div>
+                <div>BeautyStat Cosmetics</div>
+                <div>Bluemercury</div>
+            </div>
+            <div>
+                <div>Beboe Therapies</div>
+                <div>Bobbi Brown</div>
+            </div>
+        </div>
+        <div className="letterbox" id="C">C</div>
+        <hr/>
+        <div className="item_box">
+            <div>
+                <div>   <Link style={{textDecoration:"none"}} to="/caudalie">CAUDALIE</Link></div>
+                <div>Chantecaille</div>
+            </div>
+            <div>
+                <div>Costa Brazil</div>
+            </div>
+            <div>
+                <div>Christophe Robin</div>
+            </div>
+            <div>
+                <div>Clarins</div>
+            </div>
+            <div>
+                <div>Clinique</div>
+            </div>
+        </div>
+        <div className="letterbox" id="D">D</div>
+        <hr/>
+        <div className="item_box">
+            <div>
+                <div>Darphin</div>
+                <div>Dr. Barbara Sturm</div>
+            </div>
+            <div>
+                <div>Deborah Lippmann</div>
+                <div>Dr. Janine Mahon</div>
+            </div>
+            <div>
+                <div>Dermaflash</div>
+                <div>drybar</div>
+            </div>
+            <div>
+                <div>Dermalogica</div>   
+            </div>
+            <div>
+                <div>Diptyque</div>
+            </div>
+        </div>
+        <div className="letterbox" id="E">E</div>
+        <hr/>
+        <div className="item_box">
+            <div>
+                <div>Ebi</div>
+            </div>
+            <div>
+                <div>Ellis Brooklyn</div>
+            </div>
+            <div>
+                <div>Estée Lauder</div>
+            </div>
+            <div>
+                <div></div>   
+            </div>
+            <div>
+                <div></div>
+            </div>
+        </div>
+        <div className="letterbox" id="F">F</div>
+        <hr/>
+        <div className="item_box">
+            <div>
+                <div>FOREO</div>
+            </div>
+            <div>
+                <div>Fornasetti Profumi</div>
+            </div>
+            <div>
+                <div>Fresh</div>
+            </div>
+            <div>
+                <div>Fur</div>     
+            </div>
+            <div>
+                <div>Furtuna Skin</div>   
+            </div>   
+        </div>
+        <div className="letterbox"  id="G">G</div>
+        <hr/>
+        <div className="item_box">
+            <div>
+                <div>ghd</div>
+            </div>
+            <div>
+                <div>GLAMGLOW</div>
+            </div>
+            <div>
+                <div>GLISSANT</div>
+            </div>
+            <div>
+                <div>Glo Skin Beauty</div>     
+            </div>
+            <div>
+                <div></div>   
+            </div>   
+        </div>
+        <div className="letterbox"  id="H">H</div>
+        <hr/>
+        <div className="item_box">
+            <div>
+                <div>Hampton Sun</div>
+                <div>Hum Nutrition</div>
+            </div>
+            <div>
+                <div>HATCH Mama</div>
+            </div>
+            <div>
+                <div>Heretic</div>
+            </div>
+            <div>
+                <div>Hermetica Paris</div>   
+            </div>
+            <div>
+                <div>Hourglass</div>
+            </div>
+        </div>
+        <div className="letterbox"  id="I">I</div>
+        <hr/>
+        <div className="item_box">
+            <div>
+                <div>Indie Lee</div>
+            </div>
+            <div>
+                <div>iS CLINICAL</div>
+            </div>
+            <div>
+                <div></div>
+            </div>
+            <div>
+                <div></div>   
+            </div>
+            <div>
+                <div></div>
+            </div>
+        </div>
+        <div className="letterbox"  id="J">J</div>
+        <hr/>
+        <div className="item_box">
+            <div>
+                <div>Jack Black</div>
+                <div>Jo Malone London</div>
+            </div>
+            <div>
+                <div>James Read</div>
+                <div>Joanna Vargas</div>
+               
+            </div>
+            <div>
+                <div>Jane Iredale</div>
+                <div>Juice Beauty</div>
+            </div>
+            <div>
+                <div>Jenny Patinkin</div>
+            </div>
+            <div>
+                <div>Jillian Dempsey</div>
+            </div>
+        </div>
+        <div className="letterbox">K</div>
+        <hr/>
+        <div className="item_box">
+            <div>
+                <div>Kevyn Aucoin</div>
+                <div>Kosas</div>
+            </div>
+            <div>
+                <div>Kiehl's Since 1851</div>               
+            </div>
+            <div>
+                <div>Kjaer Weis</div>
+            </div>
+            <div>
+                <div>KLORANE</div>
+            </div>
+            <div>
+                <div>KNC</div>
+            </div>
+        </div>
+        <div className="letterbox">L</div>
+        <hr/>
+        <div className="item_box">
+            <div>
+                <div>La Mer</div>
+                <div>Laura Mercier</div>
+                <div>Living Proof</div>
+            </div>
+            <div>
+                <div>La Roche-Posay</div>
+                <div>LEGOLOGY</div>
+                <div>Lune+Aster</div>
+            </div>
+            <div>
+                <div>LAFCO</div>
+                <div>LightStim</div>
+            </div>
+            <div>
+                <div>Lash Star</div>
+                <div>lilah b.</div>
+            </div>
+            <div>
+                <div>LASHFOOD</div>
+                <div>Lipstick Queen</div>
+            </div>
+        </div>
+        <div className="letterbox">M</div>
+        <hr/>
+        <div className="item_box">
+            <div>
+                <div>M-61</div>
+                <div>Molton Brown</div>
+            </div>
+            <div>
+                <div>Mara</div>
+                <div>Moroccanoil</div>
+            </div>
+            <div>
+                <div>Mario Badescu</div>
+                <div>MZ SKIN</div>
+            </div>
+            <div>
+                <div>Mason Pearson</div>
+            </div>
+            <div>
+                <div>Maya Chia</div>
+            </div>
+        </div>
+        <div className="letterbox">N</div>
+        <hr/>
+        <div className="item_box">
+            <div>
+                <div>Nailmatic</div>
+                <div>Noble Panacea</div>
+            </div>
+            <div>
+                <div>Nannette De Gaspé</div>
+                <div>NuFace</div>
+            </div>
+            <div>
+                <div>NARS</div>
+            </div>
+            <div>
+                <div>Natura Bissé</div>
+            </div>
+            <div>
+                <div>NEST New York</div>
+            </div>
+        </div>
+        <div className="letterbox">O</div>
+        <hr/>
+        <div className="item_box">
+            <div>
+                <div>Odacité</div>
+            </div>
+            <div>
+                <div>Olaplex</div>
+            </div>
+            <div>
+                <div>One Love Organics</div>
+            </div>
+            <div>
+                <div>Oribe</div>
+            </div>
+            <div>
+                <div>OSEA</div>
+            </div>
+        </div>
+        <div className="letterbox">P</div>
+        <hr/>
+        <div className="item_box">
+            <div>
+                <div>Patchology</div>
+                <div>PMD</div>
+            </div>
+            <div>
+                <div>Perricone MD</div>
+            </div>
+            <div>
+                <div>Peter Thomas Roth</div>
+            </div>
+            <div>
+                <div>Philip Kingsley</div>
+            </div>
+            <div>
+                <div>PHYTO</div>
+            </div>
+        </div>
+        <div className="letterbox">R</div>
+        <hr/>
+        <div className="item_box">
+            <div>
+                <div>R+Co</div>
+                <div>Retrouvé</div>
+            </div>
+            <div>
+                <div>Rahua</div>
+                <div>RMS Beauty</div>
+            </div>
+            <div>
+                <div>Ranavat Botanics</div>
+                <div>Rodial</div>
+            </div>
+            <div>
+                <div>REN Clean Skincare</div>
+                <div>RÓEN BEAUTY</div>
+            </div>
+            <div>
+                <div>Renessence</div>
+                <div>RéVive</div>
+            </div>
+        </div>
+        <div className="letterbox">S</div>
+        <hr/>
+        <div className="item_box">
+            <div>
+                <div>Sara Happ</div>
+                <div>SkinCeuticals</div>
+                <div>Soleil Toujours</div>
+                <div>Surratt</div>
+            </div>
+            <div>
+                <div>SEEN</div>
+                <div>skyn ICELAND</div>
+                <div>SPARITUAL</div>
+            </div>
+            <div>
+                <div>SHHHOWERCAP</div>
+                <div>Slip</div>
+                <div>St. Tropez</div>
+            </div>
+            <div>
+                <div>SiO Beauty</div>
+                <div>Smith & Cul</div>
+                <div>Supergoop!</div>
+            </div>
+            <div>
+                <div>SK-II</div>
+                <div>Snow Fox Skincare</div>
+                <div>Supermood</div>
+            </div>
+        </div>
+        <div className="letterbox">T</div>
+        <hr/>
+        <div className="item_box">
+            <div>
+                <div>T3</div>
+                <div>Tata Harper</div>
+                <div>The Perfect V</div>
+                <div>Trish McEvoy</div>
+            </div>
+            <div>
+                <div>Tammy Fender</div>
+                <div>Teami</div>
+                <div>This Works</div>
+                <div>Tweezerman</div>
+            </div>
+            <div>
+                <div>Tan-Luxe</div>
+                <div>The Beauty Chef</div>
+                <div>TOCCA</div>
+            </div>
+            <div>
+                <div>TanTowel</div>
+                <div>The Light Salon</div>
+                <div>Tom Ford</div>
+            </div>
+            <div>
+                <div>Tara Smith</div>
+                <div>The Nue Co</div>
+                <div>Tracie Martyn</div>
+            </div>
+        </div>
+        <div className="letterbox">V</div>
+        <hr/>
+        <div className="item_box">
+            <div>
+                <div>Vilhelm Parfumerie</div>
+                <div>VOLO Beauty</div>
+            </div>
+            <div>
+                <div>VIRTUE</div>
+            </div>
+            <div>
+                <div>VIRTUE® Flourish</div>
+            </div>
+            <div>
+                <div>Vita Liberata</div>
+            </div>
+            <div>
+                <div>Vitruvi</div>
+            </div>
+        </div>
+        <div className="letterbox">W</div>
+        <hr/>
+        <div className="item_box">
+            <div>
+                <div>WelleCo</div>
+            </div>
+            <div>
+                <div>Wildling</div>
+            </div>
+            <div>
+                <div></div>
+            </div>
+            <div>
+                <div></div>
+            </div>
+            <div>
+                <div></div>
+            </div>
+        </div>
+        <div className="letterbox">Y</div>
+        <hr/>
+        <div className="item_box">
+            <div>
+                <div>YUNI</div>
+            </div>
+            <div>
+                <div>Yves Saint Laurent</div>
+            </div>
+            <div>
+                <div></div>
+            </div>
+            <div>
+                <div></div>
+            </div>
+            <div>
+                <div></div>
+            </div>
+        </div>
+        <div className="letterbox">#</div>
+        <hr/>
+        <div className="item_box">
+            <div>
+                <div>111SKIN</div>
+            </div>
+            <div>
+                <div>8Greens</div>
+            </div>
+            <div>
+                <div></div>
+            </div>
+            <div>
+                <div></div>
+            </div>
+            <div>
+                <div></div>
+            </div>
+        </div>
+    </div>
+    <hr/>
+    </div>
+    </>
+  )
+}
+
+export default Brands
