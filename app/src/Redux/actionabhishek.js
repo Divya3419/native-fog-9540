4b418d347f5a0f53ecde2f3598e88c31c4bba4b9
abhishekskmr44 <abhishekskmr44@gmail.com>
2022-07-21 23:41:30 +0530

    file names changed to avoid conflict
    A big lesson for mankind!

---
+++ app/src/Redux/actionabhishek.js
@@ -0,0 +1,66 @@
+import { FETCH_MYDATA_REQUEST, FETCH_MYDATA_SUCCESS, FETCH_MYDATA_FAILURE} from "./actionTypes";
+import axios from "axios"
+const fetchDataRequest= (payload) => {
+    return {
+        type: FETCH_MYDATA_REQUEST,
+        payload
+    }
+}
+
+const fetchDataSuccess=(payload) => {
+   console.log(payload)
+    return {
+        type: FETCH_MYDATA_SUCCESS,
+        payload
+    }
+}
+
+const fetchDataFailure=(payload) => {
+    return {
+        type: FETCH_MYDATA_FAILURE,
+        payload
+
+    }
+}
+
+const fetchData=(payload)=> {
+    return (dispatch) => {
+        dispatch (fetchDataRequest(payload))
+    
+   
+    axios.get("http://localhost:8080/products")
+
+    .then(res=> dispatch(fetchDataSuccess(res.data)))
+    .catch (err => dispatch(fetchDataFailure(err.data)))
+    }}
+
+
+    export {fetchData}
+    const fetchDatab=(Baby)=> {
+        return (dispatch) => {
+            dispatch (fetchDataRequest(Baby))
+        
+        
+        axios.get("http://localhost:8080/products",
+       {params:{category:Baby}
+        })
+        .then(res=> dispatch(fetchDataSuccess(res.data)))
+        .catch (err => dispatch(fetchDataFailure(err.data)))
+    }}
+
+
+
+    const fetchDataC=(pencil)=>{
+        return(dispatch)=>{
+            dispatch(fetchDataRequest(pencil))
+
+            axios.get("http://localhost:8080/products",
+            {params:{category:pencil}}
+            )
+            .then(res=>dispatch(fetchDataSuccess(res.data)))
+            .catch(err=> dispatch(fetchDataFailure(err.data)))
+
+
+        }
+    }
+    export { fetchDatab,fetchDataC}
