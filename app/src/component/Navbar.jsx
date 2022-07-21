b18bbe6bee70ce3da1ad6562f78bcd780e24fd41
Divya <99958902+Divya3419@users.noreply.github.com>
2022-07-21 20:58:45 +0530

    done

--- app/src/component/Navbar.jsx
+++ app/src/component/Navbar.jsx
@@ -1,7 +1,8 @@
 import { SearchIcon } from "@chakra-ui/icons";
-import { Box, Img } from "@chakra-ui/react";
+import { Box, Center, Img } from "@chakra-ui/react";
 import React from "react";
 import { Link as RouteLink } from "react-router-dom";
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
 const Navbar = () => {
   return (
     <>
@@ -11,7 +12,7 @@ const Navbar = () => {
         gridTemplateColumns="25% 50% 25%"
         marginTop="20px"
       >
-        <Box fontSize="12px" color="blue.400">
+        <Box fontSize="10px">
           <RouteLink to="/location">
             <Box display="flex" paddingLeft="40px">
               <Img
@@ -24,7 +25,7 @@ const Navbar = () => {
         </Box>
         <Box></Box>
 
-        <Box fontSize="12px" display="grid" gridTemplateColumns="repeat(4,1fr)">
+        <Box fontSize="10px" display="grid" gridTemplateColumns="repeat(4,1fr)">
           <Box display="flex" gap="10px">
             <Img
               width="30%"
@@ -56,19 +57,17 @@ const Navbar = () => {
         </Box>
       </Box>
 
-      <Box height="40px" display="grid" gridTemplateColumns="repeat(3,1fr)">
-        <Box></Box>
-        <Box>
-          <a href=""><Img
-            width="40%"
+      
+
+<Center marginBottom="10px" h='40px'>
+<a href=""><Img
+            width="15%"
             margin="auto"
             src="https://cdn.shopify.com/s/files/1/0283/0185/2747/files/bluemercury-logo_1216x.png?v=1648743182"
           /></a>
-        </Box>
-        <Box></Box>
-      </Box>
+</Center>
 
-      <Box height="40px" display="grid" gridTemplateColumns="15% 70% 15%">
+       <Box height="40px" display="grid" gridTemplateColumns="15% 70% 15%">
         <Box></Box>
         <Box display="grid" fontSize="12px" gridTemplateColumns="repeat(7,1fr)">
           <Box>SHOP</Box>
@@ -80,7 +79,115 @@ const Navbar = () => {
           <Box>BEAUTY ALFRESCO</Box>
         </Box>
         <Box></Box>
+      </Box> 
+{/* <Tabs fontSize="12px" width="80%" margin="auto" justifyContent="center">
+  <TabList>
+    <Tab>SHOP</Tab>
+    <Tab>NEW!</Tab>
+    <Tab>BRAND</Tab>
+    <Tab>EXPLORE</Tab>
+    <Tab>EVENT</Tab>
+    <Tab>BLUEREWARDS</Tab>
+    <Tab>BEAUTY ALFRESCO</Tab>
+    
+  </TabList>
+
+  <TabPanels>
+    <TabPanel>
+      <Box width="80%" margin="auto" display="grid" gridTemplateColumns="30% 70%" color="grey" fontSize="12px" border="2px solid black">
+        <Box>
+          <p>MAKE-UP</p>
+          <p>HAIR</p>
+          <p>BATH & BODY</p>
+          <p>HOME</p>
+          <p>SUN CARE</p>
+          
+        </Box>
+        <Box></Box>
       </Box>
+
+    </TabPanel>
+    <TabPanel>
+    <Box width="80%" margin="auto" display="grid" gridTemplateColumns="30% 70%" color="grey" fontSize="12px" border="2px solid black">
+        <Box>
+          <p>MAKE-UP</p>
+          <p>HAIR</p>
+          <p>BATH & BODY</p>
+          <p>HOME</p>
+          <p>SUN CARE</p>
+          
+        </Box>
+        <Box></Box>
+      </Box>
+    </TabPanel>
+    <TabPanel>
+    <Box width="80%" margin="auto" display="grid" gridTemplateColumns="30% 70%" color="grey" fontSize="12px" border="2px solid black">
+        <Box>
+          <p>MAKE-UP</p>
+          <p>HAIR</p>
+          <p>BATH & BODY</p>
+          <p>HOME</p>
+          <p>SUN CARE</p>
+          
+        </Box>
+        <Box></Box>
+      </Box>
+    </TabPanel>
+    <TabPanel>
+    <Box width="80%" margin="auto" display="grid" gridTemplateColumns="30% 70%" color="grey" fontSize="12px" border="2px solid black">
+        <Box>
+          <p>MAKE-UP</p>
+          <p>HAIR</p>
+          <p>BATH & BODY</p>
+          <p>HOME</p>
+          <p>SUN CARE</p>
+          
+        </Box>
+        <Box></Box>
+      </Box>
+    </TabPanel>
+    <TabPanel>
+    <Box width="80%" margin="auto" display="grid" gridTemplateColumns="30% 70%" color="grey" fontSize="12px" border="2px solid black">
+        <Box>
+          <p>MAKE-UP</p>
+          <p>HAIR</p>
+          <p>BATH & BODY</p>
+          <p>HOME</p>
+          <p>SUN CARE</p>
+          
+        </Box>
+        <Box></Box>
+      </Box>
+    </TabPanel>
+    <TabPanel>
+    <Box width="80%" margin="auto" display="grid" gridTemplateColumns="30% 70%" color="grey" fontSize="12px" border="2px solid black">
+        <Box>
+          <p>MAKE-UP</p>
+          <p>HAIR</p>
+          <p>BATH & BODY</p>
+          <p>HOME</p>
+          <p>SUN CARE</p>
+          
+        </Box>
+        <Box></Box>
+      </Box>
+    </TabPanel>
+    <TabPanel>
+    <Box width="80%" margin="auto" display="grid" gridTemplateColumns="30% 70%" color="grey" fontSize="12px" border="2px solid black">
+        <Box>
+          <p>MAKE-UP</p>
+          <p>HAIR</p>
+          <p>BATH & BODY</p>
+          <p>HOME</p>
+          <p>SUN CARE</p>
+          
+        </Box>
+        <Box></Box>
+      </Box>
+    </TabPanel>
+  </TabPanels>
+</Tabs>
+ */}
       <Box
         border="2px solid black"
         backgroundColor="black"
