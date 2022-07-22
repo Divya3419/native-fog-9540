0c382832fbfe5db7ba2995191ada2b72eabd736e
NIKHIL HIRULKAR <97525857+nikhilhir@users.noreply.github.com>
2022-07-23 01:55:15 +0530

    day4

--- app/src/Pages/MainRoute.jsx
+++ app/src/Pages/MainRoute.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import AddressDetails from '../component/AddressDetails'
 import Cart from '../component/Cart'
 import Footer from '../component/Footer'
 import Navbar from '../component/Navbar'
+import PaymentPage from '../component/PaymentPage'
+import Success from '../component/Success'
 import Abel from './Abel'
 import BabyFoot from './BabyFoot'
 import Brands from './Brands'
@@ -16,9 +19,16 @@ import NewProducts from './NewProducts'
 
 
 const MainRoute = () => {
+  const location = useLocation() 
   return (
     <>
-    <Navbar/>
+
+
+    { 
+      location.pathname === "/Success" ? null : (
+        <Navbar />
+      )}
+    
     <Routes>
 
 
@@ -32,12 +42,28 @@ const MainRoute = () => {
     <Route path="/babyfoot" element={<BabyFoot/>}/>
     <Route path="/caudalie" element={<Caudalie/>}/>
     <Route path="/brands" element={<Brands/>}/>
-  
+    
+    <Route path="/Success" element={<Success/>}/>
     <Route path="/NewProducts" element={<NewProducts/>}/>
     <Route path="/Cart" element={<Cart/>}/>
+    <Route path="/AddressDetails" element={<AddressDetails/>}/>
+    <Route path="/PaymentPage" element={<PaymentPage/>}/>
+   
 
     </Routes>
+
+    {
+      location.pathname === "/PaymentPage" ||
+      location.pathname === "/Success" ? null : (
+        <>
+          
+
     <Footer/>
+    </>
+
+   ) }
+    
+    
     </>
   )
 }
