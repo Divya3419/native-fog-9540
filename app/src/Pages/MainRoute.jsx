bb900e3b612448f2c4ceff29e07e8b082ef9d5bf
Aun Mohammad <99705070+aun0545088@users.noreply.github.com>
2022-07-23 07:55:40 +0530

    updt

--- app/src/Pages/MainRoute.jsx
+++ app/src/Pages/MainRoute.jsx
@@ -1,20 +1,18 @@
-
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import Navbar from "../component/Navbar";
+import Home from "./Home";
+import Login from "./Login";
+import Register from "./Register";
+import Location from "./Location";
 import Abel from "./Abel";
 import BabyFoot from "./BabyFoot";
-import Brands from "./Brands";
 import Caudalie from "./Caudalie";
-import Home from "./Home";
-import Location from "./Location";
-import Login from "./Login";
+import Brands from "./Brands";
 import NewProducts from "./NewProducts";
-import Register from "./Register";
-import Cart from '../component/Cart'
-import Footer from '../component/Footer'
-import Navbar from '../component/Navbar'
-import ProductDetail from './ProductDetail'
-
+import ProductDetail from "./ProductDetail";
+import Cart from "../component/Cart";
+import Footer from "../component/Footer";
 
 const MainRoute = () => {
   return (
@@ -22,23 +20,20 @@ const MainRoute = () => {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/register' element={<Register/>}
-    <Route path="/location" element={<Location/>}/>
-    <Route path="/abel" element={<Abel/>}/>
-    <Route path="/babyfoot" element={<BabyFoot/>}/>
-    <Route path="/caudalie" element={<Caudalie/>}/>
-    <Route path="/brands" element={<Brands/>}/>
-  
-
-    <Route path="/newproducts" element={<NewProducts/>}/>
-    <Route path="/productdetail/:id" element={<ProductDetail/>}/>
-
-    <Route path="/Cart/:id" element={<Cart/>}/>
-
-    </Routes>
-    <Footer/>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/location" element={<Location />} />
+        <Route path="/abel" element={<Abel />} />
+        <Route path="/babyfoot" element={<BabyFoot />} />
+        <Route path="/caudalie" element={<Caudalie />} />
+        <Route path="/brands" element={<Brands />} />
+        <Route path="/newproducts" element={<NewProducts />} />
+        <Route path="/productdetail/:id" element={<ProductDetail />} />
+        <Route path="/Cart/:id" element={<Cart />} />
+      </Routes>
+      <Footer />
     </>
   );
 };
+
 export default MainRoute;
