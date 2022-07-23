f87821374c2457e9422866eb62e0e57e6150e6a0
abhishekskmr44 <abhishekskmr44@gmail.com>
2022-07-23 18:19:06 +0530

    done

--- app/src/Pages/NewProducts.jsx
+++ app/src/Pages/NewProducts.jsx
@@ -3,11 +3,34 @@ import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
-import {  fetchmyData } from '../Redux/action.js';
+import {  addCityReducer, fetchmyData } from '../Redux/action.js';
+import {Box, Button} from "@chakra-ui/react"
+//sorting algorithm import
 
-import {Box} from "@chakra-ui/react"
+
+
+
+
+
+
 
 const NewProducts = () => {
+
+  //sorting algorithm
+
+  const sortHandler = (e) => {
+    console.log(e.target.value,"sortHandler")
+    if(e.target.value=="asc")
+        //dispatch(sorting(1))
+        dispatch(addCityReducer(1))
+    else if(e.target.value=="dec")
+        //dispatch(sorting(-1))
+        dispatch(addCityReducer(-1))
+
+        else
+        dispatch(addCityReducer(1))
+
+}
 
     const nav=useNavigate()
   const dispatch = useDispatch();
@@ -22,16 +45,40 @@ const NewProducts = () => {
 
 
     return (
-    <Box border="2px solid black" display = "flex" >
-    <Box className='all-product' border="2px solid black" display = "grid" gridTemplateColumns="repeat(3,1fr)">
+      <>
+     
+      <Box className="sort-Box">
+      <Button>
+    <select onChange={sortHandler} name="" id="sort">
+    <option value="sel">--SORT BY PRICING--</option>
+    <option value="asc">Low to High</option>
+    <option value="dec">High to Low</option>
+    </select>  
+      </Button>
+      </Box>
+   
+    
+    <Box className='all-product'  display = "grid" gridTemplateColumns="repeat(5,1fr)">
      {ProductData2.map((e) => {
         const { title,name,image,image_link,brand, price, description, category,  id, rating } = e;
+
+
+         
     
         return (
-     
-          <Box  className="prt-Box" key={id} >
+           
+          
+         
+          
+          <Box className="prt-Box" key={id} >
+         
+         
+          {/* sorting algorithm inside sort-Box */}
+           
+       
 
-            <img src={image_link}></img>
+
+            <img src={image_link}/>
             <Box className="view">
               <Box className="view-icon" onClick={ ()=>{ nav(`/product/${id}`)}}></Box>
               {/* < FavoriteBorderOutlinedIcon className="love-icon-1" onClick={ ()=>{ Addtowishlist( id) }}/> */}
@@ -42,10 +89,17 @@ const NewProducts = () => {
        
             
           </Box>
+          
+
+         
         );
-      })}
+        
+      })
+     
+    }
     </Box>
-    </Box>
+  
+    </>
   )
 }
 
